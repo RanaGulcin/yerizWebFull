ecb582bdf481daf829b0ff6905bd0b60f2713d3b
RanaGulcin <ranagulcin@gmail.com>
2024-02-28 20:11:06 +0300

    index

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -3,11 +3,9 @@ import Layout from '../layout/client/layout/Layout';
 import Home from '../pages/client/homePage/Home';
 import About from '../pages/client/aboutPage/About';
 import Blog from '../pages/client/blogPage/Blog';
-<<<<<<< HEAD
 import BlogDetail from '../pages/client/blogDetailPage/BlogDetail';
-=======
 import YerizApp from '../pages/client/yerizappPage/YerizApp';
->>>>>>> 1cbb12182a96efd29bd84f1c331dac9475c2fc50
+
 
 
 const routes = createBrowserRouter([
